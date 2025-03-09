91cee632a0adc6aea63cea1f2a7b7737688c40fe
tsots <mosalatsotetsi@gmail.com>
2025-03-09 17:45:13 +0200

    we see basePath and Prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  basePath: "/classic",
+  // basePath: "/classic",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? "/classic/" : "",
+  basePath: isProd ? "/classic" : "",
   output: "export",
 };
 
