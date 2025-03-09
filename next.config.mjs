f566d522f8d35589b42bc7c07181e654a2b6e8cf
tsots <mosalatsotetsi@gmail.com>
2025-03-09 18:48:45 +0200

    next images

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
-  basePath: "/classic",
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
+  basePath: isProd ? "/classic" : "",
+
+  // reactStrictMode: true,
+  // images: {
+  //   unoptimized: true, // Disable default image optimization
+  // },
+  // trailingSlash: true,
+  // skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
