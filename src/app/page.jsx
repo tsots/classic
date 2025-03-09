143a80622a706a42fa73d16f8b8878b8675357a9
tsots <mosalatsotetsi@gmail.com>
2025-03-09 19:02:21 +0200

    removed homepage package,json

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,13 +4,21 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
+        {/* <Image */}
+        {/*   className="dark:invert" */}
+        {/*   src="/next.svg" */}
+        {/*   alt="Next.js logo" */}
+        {/*   width={180} */}
+        {/*   height={38} */}
+        {/*   priority */}
+        {/* /> */}
+
+        <img
           className="dark:invert"
           src="/next.svg"
-          alt="Next.js logo"
           width={180}
           height={38}
-          priority
+          alt="what's up"
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
